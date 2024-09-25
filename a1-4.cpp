8eaabe9b301448952b83e22b59a52dccb4f8a137
krish-047 <150154237+krish-047@users.noreply.github.com>
2024-09-25 23:51:19 +0530

    Update a1-4.cpp

--- a1-4.cpp
+++ a1-4.cpp
@@ -2,53 +2,134 @@
 
 using namespace std;
 
-//write your code here
+// Stack class definition
+class Stack {
+    int* arr;
+    int top;
+    int capacity;
 
-// Function to find the median
-double findMedian(int arr1[], int size1, int arr2[], int size2) {
-    
-// Your code here
+public:
+    Stack(int size) {
+        arr = new int[size];
+        capacity = size;
+        top = -1;
+    }
 
+    ~Stack() {
+        delete[] arr;
+    }
+
+    // Function to add an element to the stack
+    void push(int x) {
+        if (isFull()) {
+            cout << "Stack Overflow: Unable to push " << x << endl;
+            return;
+        }
+        arr[++top] = x;
+    }
+
+    // Function to pop the top element
+    int pop() {
+        if (isEmpty()) {
+            cout << "Stack Underflow: Unable to pop" << endl;
+            return -1; // Return a sentinel value for error
+        }
+        return arr[top--];
+    }
+
+    // Function to display the elements of the stack
+    void display() const {
+        if (isEmpty()) {
+            cout << "Empty";
+        } else {
+            for (int i = 0; i <= top; i++) {
+                cout << arr[i] << " ";
+            }
+        }
+        cout << endl;
+    }
+
+private:
+    // Function to check if the stack is full
+    bool isFull() const {
+        return top == capacity - 1;
+    }
+
+    // Function to check if the stack is empty
+    bool isEmpty() const {
+        return top == -1;
+    }
+};
+
+// Stacks for the towers
+Stack* A;
+Stack* B;
+Stack* C;
+
+// Function to display the contents of the towers
+void displayTowers() {
+    cout << "Tower A: ";
+    A->display();
+    cout << "Tower B: ";
+    B->display();
+    cout << "Tower C: ";
+    C->display();
+    cout << "\n";
 }
 
+// Recursive function to rearrange disks
+void rearrangeDisks(int n, Stack& source, Stack& target, Stack& auxiliary, char from, char to, char aux) {
+    if (n == 1) {
+        // Move the disk from source to target
+        int disk = source.pop();
+        target.push(disk);
+        cout << "Move disk " << disk << " from " << from << " to " << to << endl;
+        displayTowers();
+        return;
+    }
+    
+    // Move n-1 disks from source to auxiliary
+    rearrangeDisks(n - 1, source, auxiliary, target, from, aux, to);
+    
+    // Move the nth disk from source to target
+    int disk = source.pop();
+    target.push(disk);
+    cout << "Move disk " << disk << " from " << from << " to " << to << endl;
+    displayTowers();
+
+    // Move the n-1 disks from auxiliary to target
+    rearrangeDisks(n - 1, auxiliary, target, source, aux, to, from);
+}
 
 int main() {
-    // You can use cout statements, but ensure that you submit after commenting them out, as the autograder will otherwise mistake it for your actual output
-    // Everything else in the main function remains unchanged
-    
-    int size1;
-    // cout << "Enter the size of the array1: ";
-    cin >> size1;
+    int n;
+    //cout << "Enter the number of disks: ";
+    cin >> n;
 
-    int arr[size1];
-    // cout << "Enter " << size1 << " elements:" << endl;
-    for (int i = 0; i < size1; ++i) {
-        cin >> arr[i];
+    if (cin.fail() || n <= 0) {
+        cout << "Invalid input. Please enter a positive integer." << endl;
+        return 1;
     }
 
-    // cout << "The array elements are: ";
-    // for (int i = 0; i < size1; ++i) {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << endl;
+    else
+    {
 
-    int size2;
-    // cout << "Enter the size of the array2: ";
-    cin >> size2;
+      A = new Stack(n);
+      B = new Stack(n);
+      C = new Stack(n);
 
-    int arr1[size2];
-    // cout << "Enter " << size2 << " elements:" << endl;
-    for (int i = 0; i < size2; ++i) {
-        cin >> arr1[i];
+      for (int i = n; i >= 1; i--) {
+        A->push(i);
+      }
+
+      displayTowers();
+
+      rearrangeDisks(n, *A, *C, *B, 'A', 'C', 'B');
+
+      delete A;
+      delete B;
+      delete C;
+
+     return 0;
     }
-
-    // cout << "The array elements are: ";
-    // for (int i = 0; i < size2; ++i) {
-    //     cout << arr1[i] << " ";
-    // }
-    // cout << endl;
-    
-    cout << "Median = " << findMedian(arr, size1, arr1, size2) << endl; // do not comment this out
-    
-    return 0;
 }
